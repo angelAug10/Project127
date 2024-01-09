759778acf3f9c82eccd16586c833cbe6134d7f4b
angelAug10 <128250923+angelAug10@users.noreply.github.com>
2024-01-08 17:13:52 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+module.exports = { temp };
+const { temp } = require("pathOfFirstFile.js");
+function preload() {
+    img = loadImage('assets/laDefense.jpg');
+  }
+  function setup() {
+    createCanvas(390, 240);
+    capture = createCapture(VIDEO);
+    capture.size(320, 240);
+    video.hide();
+  }
+  function draw() {
+    background(255);
+    image(capture, 0, 0, 320, 240);
+    filter(INVERT);
+  }
